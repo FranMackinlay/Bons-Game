1b9607ad494914d061a3dfe5d6f7375b6a86201c
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-10 16:31:00 -0300

    App re format to class

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Login from './components/login/login';
 import GameBoard from './components/game-board/gameBoard';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/gameBoard" component={GameBoard} />
-        <Route to="/Login" component={Login} />
-      </Switch>
-    </Router>
-  );
-}
+export default class App extends Component {
+  render() {
 
-export default App;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/gameBoard" component={GameBoard} />
+          <Route to="/Login" component={Login} />
+        </Switch>
+      </Router >
+    );
+  }
+}
