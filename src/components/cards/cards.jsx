2f660722b29779d8bd75ae647e07b08b73bc1413
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-10 16:50:40 -0300

    cards component created

---
+++ src/components/cards/cards.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './cards.css';
+
+export default class Cards extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div class='card'>
+				<img src='https://www.mathcs.emory.edu/~cheung/Courses/170/Syllabus/10/FIGS/0/As.png' class='card-img-top' alt='As'></img>
+				<div class='card-body'>
+					<p class='card-text'>Some quick example.</p>
+				</div>
+			</div>
+		);
+	}
+}
