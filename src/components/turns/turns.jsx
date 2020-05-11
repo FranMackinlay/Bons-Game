a111e06e903718f6d06fefed7219a28a071d9691
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-11 13:14:36 -0300

    turns component created

---
+++ src/components/turns/turns.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './turns.css';
+
+export default class Turns extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentTurn: this.props.game.currentTurn,
+			turnsLeft: this.props.game.turnsLeft,
+		};
+	}
+
+	onSubmit = async event => {
+		event.preventDefault();
+		console.log('submittt');
+	};
+
+	render() {
+		const { currentTurn, turnsLeft } = this.state;
+		return (
+			<form className='turns-info' onSubmit={this.onSubmit}>
+				<div className='info'>
+					<div className='current-turn info-state'>
+						<h4>Current Turn:</h4>
+						<h5>{currentTurn}</h5>
+					</div>
+					<div className='turns-left info-state'>
+						<h4>Turns Left:</h4>
+						<h5>{turnsLeft}</h5>
+					</div>
+				</div>
+				<button className='submit' type='submit'>
+					End Turn
+				</button>
+			</form>
+		);
+	}
+}
