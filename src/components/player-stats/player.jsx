81f0d92ca27a4428b74d37609ccd79e0c4f931df
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-11 12:12:14 -0300

    player component finished

--- src/components/player-stats/player.jsx
+++ src/components/player-stats/player.jsx
@@ -4,19 +4,31 @@ import './player.css';
 export default class Players extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			name: this.props.name,
+			hp: this.props.player.hp,
+			maxHp: this.props.player.maxHp,
+			shield: this.props.player.shield,
+		};
 	}
 
 	render() {
+		const { name, hp, shield, maxHp } = this.state;
 		return (
-			<div id='player' class='card mb-3'>
-				<div class='row no-gutters'>
-					<div class='user col-md-4'>
-						<i class='fas fa-user'></i>
+			<div id='player' className='card mb-3'>
+				<div className='row no-gutters'>
+					<div className='user col-md-4'>
+						<i className='fas fa-user'></i>
 					</div>
-					<div class='col-md-8'>
-						<div class='card-body'>
-							<h5 class='card-title'>Card title</h5>
-							<p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<div className='col-md-8'>
+						<div className='card-body'>
+							<h5 className='card-title'>{name}</h5>
+							<div className='hp-shield'>
+								<h6 className='card-text'>
+									HP: {hp}/{maxHp}
+								</h6>
+								<h6 className='card-text'>Shield: {shield}</h6>
+							</div>
 						</div>
 					</div>
 				</div>
