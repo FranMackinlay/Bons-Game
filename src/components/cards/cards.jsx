7051b8fac30bc1f7a538e88465fbd0740cd58059
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-14 10:04:01 -0300

    cards refactor

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './cards.css';
 
-export default class Cards extends Component {
-	render() {
-		const { effect, value } = this.props;
-		return (
-			<div id='card' className='card'>
-				<h4>{effect}</h4>
-				<div className='card-body'>
-					<h5 className='card-text'>Value: {value}</h5>
-				</div>
+export default function Cards({ effect, value }) {
+	return (
+		<div id='card' className='card'>
+			<h4>{effect}</h4>
+			<div className='card-body'>
+				<h5 className='card-text'>Value: {value}</h5>
 			</div>
-		);
-	}
+		</div>
+	);
 }
