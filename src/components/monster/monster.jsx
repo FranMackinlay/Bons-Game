540bb42c120faee109c3955bde2e7d2836df750e
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-14 10:08:35 -0300

    refactor

--- src/components/monster/monster.jsx
+++ src/components/monster/monster.jsx
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Monster extends Component {
-	render() {
-		const { name, hp, shield, maxHp } = this.props.monster;
-		return (
-			<div id='monster' className='card'>
-				<div className='row no-gutters'>
-					<div className='user col-md-4'>
-						<i className='fas fa-user'></i>
-					</div>
-					<div className='col-md-8'>
-						<div className='card-body'>
-							<h5 className='card-title'>{name}</h5>
-							<div className='hp-shield'>
-								<h6 className='card-text'>
-									HP: {hp}/{maxHp}
-								</h6>
-								<h6 className='card-text'>Shield: {shield}</h6>
-							</div>
+export default function Monster({ monster: { name, hp, shield, maxHp } }) {
+	return (
+		<div id='monster' className='card'>
+			<div className='row no-gutters'>
+				<div className='user col-md-4'>
+					<i className='fas fa-user'></i>
+				</div>
+				<div className='col-md-8'>
+					<div className='card-body'>
+						<h5 className='card-title'>{name}</h5>
+						<div className='hp-shield'>
+							<h6 className='card-text'>
+								HP: {hp}/{maxHp}
+							</h6>
+							<h6 className='card-text'>Shield: {shield}</h6>
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
