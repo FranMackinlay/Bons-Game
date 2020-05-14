a789f8d2ecfc1e9fa3e7f95bcf32ca3d1b3c3879
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-14 10:10:23 -0300

    refactor

--- src/components/player-stats/player.jsx
+++ src/components/player-stats/player.jsx
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './player.css';
 
-export default class Players extends Component {
-	render() {
-		const { name, hp, shield, maxHp } = this.props.player;
-		return (
-			<div className='player-cards mb-3'>
-				<div id='player' className='card'>
-					<div className='row no-gutters'>
-						<div className='user col-md-4'>
-							<i className='fas fa-user'></i>
-						</div>
-						<div className='col-md-8'>
-							<div className='card-body'>
-								<h5 className='card-title'>{name}</h5>
-								<div className='hp-shield'>
-									<h6 className='card-text'>
-										HP: {hp}/{maxHp}
-									</h6>
-									<h6 className='card-text'>Shield: {shield}</h6>
-								</div>
+export default function Players({ player: { name, hp, shield, maxHp } }) {
+	return (
+		<div className='player-cards mb-3'>
+			<div id='player' className='card'>
+				<div className='row no-gutters'>
+					<div className='user col-md-4'>
+						<i className='fas fa-user'></i>
+					</div>
+					<div className='col-md-8'>
+						<div className='card-body'>
+							<h5 className='card-title'>{name}</h5>
+							<div className='hp-shield'>
+								<h6 className='card-text'>
+									HP: {hp}/{maxHp}
+								</h6>
+								<h6 className='card-text'>Shield: {shield}</h6>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
